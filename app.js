6f91fbafd491ac8267697c75113e6795f8a81b22
Damien-Ar <damien.aranda42@gmail.com>
2024-05-31 16:01:35 +0200

    passage sur docker classique

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-import { v2 as compose } from "docker-compose";
+import {Docker} from 'node-docker-api';
+import express from 'express';
 
-compose.upOne("web", { cwd: ".", log: true})
-const result = await compose.ps({ cwd: ".", commandOptions: [["--format", "json"]] })
-result.data.services.forEach((service) => {
-  console.log(service.name, service.command, service.state, service.ports)
-  // state is one of the defined states: paused | restarting | removing | running | dead | created | exited
-})
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'assets' folder
+app.use(express.static('assets'));
+
+app.get('/docker/containers', async (req, res) => {
+  try {
+    console.log("request container list")
+    docker.container.list({'all': true})
+    .then(containers => Promise.all(containers.map(container => container.status())))
+    .then(containersStatus => containersStatus.map(status => {return {name: status.data.Name, status: status.data.State.Status}}))
+    .then(data => res.json(data))
+    .catch(error => console.log(error));
+  } catch (error) {
+     res.status(500).send('Error fetching Docker containers');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+ 
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+ 
